c49929b658a54b745c3b35670e66946beaed5c84
tarekbadr <tarekbadrshalaan@gmail.com>
2018-10-02 15:35:54 +0200

    add more features

--- depone.go
+++ depone.go
@@ -9,3 +9,8 @@ func GetData() string {
 	log.Println("fix pug && become v1.0.1")
 	return "Hi from gomodulesdepone v1.0.1"
 }
+
+// GetExtraData : return gomodulesdepone v1.1.1.
+func GetExtraData() string {
+	return "GetExtraData from gomodulesdepone v1.1.1"
+}
